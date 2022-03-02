d717136269ec2d672c4c4a1a02f3239278f2a4a5
emagoediv <emagoedivplay@gmail.com>
2022-03-02 17:54:17 -0300

    Added updates from js for a function of pause

--- js/timer.js
+++ js/timer.js
@@ -1,41 +1,83 @@
 const BTN = document.getElementById("btn");
 const TIME = document.getElementById("time");
 const CIRCLE = document.getElementById("progressCircle");
+const CIRCLESTOP = document.getElementById("progressCircleStop");
 
 timerCount = "";
 segundos = 1500;
-let pausar = false;
 vez = 1;
-
+metadeVez = 0.5;
+pausar = false;
 BTN.addEventListener("click", () => {
+    playPomodoroTimer();
+});
+
+function playPomodoroTimer() {
     if (pausar == true) {
         window.clearInterval(timerCount);
     } else if (pausar == false) {
-        goTimer(pausar);
+        goTimer(pausar, 1500);
     }
     if (pausar == false) {
         pausar = true;
     } else {
         pausar = false;
     }
-});
+}
 
-function goTimer(pausado) {
+function goTimer(pausado, timeS, stopTime = false) {
     timerCount = setInterval(() => {
-        let time = segundosParaMinutos(segundos);
+        let time = segundosParaMinutos(timeS);
         TIME.innerHTML = time;
-        segundos--;
-        if (segundos < 0) {
+        timeS--;
+        if (timeS < 0) {
             window.clearInterval(timerCount);
+            if (stopTime == false) {
+                stopTimer();
+            }
         }
 
-        if (segundos == 1500 - 15 * vez) {
-            vez++;
-            CIRCLE.style.strokeDashoffset = 440 - (440 * vez) / 100;
+        if (stopTime == false) {
+            if (timeS == 1500 - 15 * vez) {
+                vez++;
+                metadeVez += 0.5;
+                CIRCLE.style.strokeDashoffset = 440 - (440 * vez) / 100;
+                console.log(440 - (440 * metadeVez) / 100);
+                console.log(440 - 15 * vez);
+            }
+        }
+        if (stopTime == true) {
+            if (timeS == 300 - 3 * vez) {
+                vez++;
+                metadeVez += 0.5;
+                CIRCLESTOP.style.strokeDashoffset = 440 - (440 * vez) / 100;
+                console.log(440 - (440 * metadeVez) / 100);
+                console.log(440 - 15 * vez);
+            }
+            if (timeS < 0) {
+                if (stopTime == false) {
+                    goTimer(pausar, 1500);
+                } else {
+                    window.clearInterval(timerCount);
+                    pausar = false;
+                    playPomodoroTimer();
+                    CIRCLE.style.strokeDashoffset = 440;
+                    CIRCLESTOP.style.strokeDashoffset = 440;
+                }
+            }
+        }
+
+        if (vez == 100) {
+            vez = 1;
+            metadeVez = 0.5;
         }
 
         console.log(time);
-    }, 10);
+    }, 100);
+}
+
+function stopTimer() {
+    goTimer(false, 300, true);
 }
 
 function segundosParaMinutos(segundos) {
